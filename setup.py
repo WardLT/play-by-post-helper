17e4762e116abacae4829bed8761dbe8a031a6be
Logan Ward <ward.logan.t@gmail.com>
2020-03-29 15:19:30 -0500

    Added a Travis yml file, made modron installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='modron',
+    version='0.0.1',
+    packages=find_packages(),
+    description='Bot to help manage play-by-post campaigns hosted on Slack',
+    install_requires=[
+        'flask>=1.1',
+        'humanize',
+        'slackclient',
+        'slackeventsapi'
+    ]
+)
