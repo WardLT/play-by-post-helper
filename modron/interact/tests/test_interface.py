61c698a8743e0afedc1473fdeee7ee4fc9ee0124
Logan Ward <ward.logan.t@gmail.com>
2020-04-25 12:25:24 -0500

    Print reason for skipping as log message

--- modron/interact/tests/test_interface.py
+++ modron/interact/tests/test_interface.py
@@ -68,7 +68,7 @@ def test_handle(parser, payload, caplog):
     with caplog.at_level(logging.INFO):
         assert handle_slash_command(payload, parser) == {"response_type": "in_channel"}
         sleep(5)  # Waits for the delayed thread to run
-    assert '1d20' in caplog.messages[-1]
+    assert '1d20' in caplog.messages[-2]
 
 
 def test_shortcut(parser, payload, caplog):
@@ -77,7 +77,7 @@ def test_shortcut(parser, payload, caplog):
     with caplog.at_level(logging.INFO):
         assert handle_slash_command(payload, parser) == {"response_type": "in_channel"}
         sleep(5)  # Waits for the delayed thread to run
-    assert '1d20' in caplog.messages[-1]
+    assert '1d20' in caplog.messages[-2]
 
 
 def test_roll_help(parser):
@@ -92,23 +92,24 @@ def test_rolling(client, parser, payload, caplog, tmp_config):
     args = parser.parse_args(['roll', '1d6+1'])
     with caplog.at_level(logging.INFO):
         args.interact(args, payload)
-    assert '1d6+1' in caplog.messages[0]
+    assert '1d6+1' in caplog.messages[-2]
 
     # Try rolling with a purpose
     args = parser.parse_args(['roll', '1d6+1', 'test', 'roll'])
     with caplog.at_level(logging.INFO):
         args.interact(args, payload)
-    assert '1d6+1 for test roll.' in caplog.messages[-1]
+    assert '1d6+1 for test roll.' in caplog.messages[-2]
 
     # Try rolling at advantage
     args = parser.parse_args(['roll', '1d6+1', 'test', '-a'])
     with caplog.at_level(logging.INFO):
         args.interact(args, payload)
-    assert '1d6+1 at advantage for test.' in caplog.messages[-1]
+    assert '1d6+1 at advantage for test.' in caplog.messages[-2]
 
     # Make sure the log file does not yet exist
     print(f'Checking for existence of {config.DICE_LOG}')
     assert not os.path.isfile(config.DICE_LOG)
+    assert 'skipped channel - True' in caplog.messages[-1]
 
     # Run a test with ic_all to see if it saves the log
     payload.channel_id = client.get_channel_id('ic_all')
