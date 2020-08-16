261f10ee268ce8b6c4a8364b1c60276e3afc95a8
Logan Ward <WardLT@users.noreply.github.com>
2020-08-15 23:09:39 -0500

    Clarify documentation

--- start-service.sh
+++ start-service.sh
@@ -7,5 +7,5 @@ export CLIENT_SECRET=
 export CLIENT_ID=
 export FLASK_APP=server.py
 
-# Uncomment if you use local certifications (e.g., and not certbot)
+# Uncomment next line if you use local certificates (e.g., and not ngrok)
 flask run --host 0.0.0.0 --port 7879 # --cert $CERTBOT_DIR/fullchain.pem --key $CERTBOT_DIR/privkey.pem
