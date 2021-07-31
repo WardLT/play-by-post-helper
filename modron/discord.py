938660bd48f7f8dbf55c95bd5b495c03b0744d56
Logan Ward <ward.logan.t@gmail.com>
2021-07-31 15:24:02 -0400

    Warn if bot lacks access to a channel

--- modron/discord.py
+++ modron/discord.py
@@ -4,23 +4,9 @@ from datetime import datetime
 import logging
 import re
 
-from discord import Guild, TextChannel, Message, Member, Client
-
+from discord import Guild, TextChannel, Message, Member, Forbidden
 
 logger = logging.getLogger(__name__)
-
-
-class ModronClient(Client):
-    """Client used to connect to Discord"""
-
-    async def on_ready(self):
-        logger.info(f'Logged on as {self.user}')
-
-    async def on_disconnect(self):
-        logger.warning('Disconnected from Discord service')
-
-    async def on_connect(self):
-        logger.info('Connected to Discord service')
 
 
 def match_channels_to_regex(guild: Guild, pattern: str) -> List[TextChannel]:
@@ -45,7 +31,12 @@ async def get_last_activity(channel: TextChannel) -> Optional[Tuple[datetime, Op
         - Whether the message was from the bot user
     """
 
-    message: Message = await channel.history(limit=1, oldest_first=False).get()
+    try:
+        message: Message = await channel.history(limit=1, oldest_first=False).get()
+    except Forbidden:
+        logger.warning(f'Bot lacks access to channel: {channel.name}')
+        # TODO (wardlt): Change to a timestamp of zero once we start using Discord
+        return datetime.now(), None
     if message is not None:
         return message.created_at, message.author
 
