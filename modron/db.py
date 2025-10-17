01c533ce6089d6b9f018cc92af72e0b88c88a061
Logan Ward <ward.logan.t@gmail.com>
2025-10-16 20:29:34 -0400

    Changing validators around

--- modron/db.py
+++ modron/db.py
@@ -15,7 +15,7 @@ import json
 
 import yaml
 from discord import Message
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field, field_validator
 
 from modron.characters import Character, load_character, list_available_characters
 from modron.config import config
@@ -51,13 +51,14 @@ class ModronState(BaseModel):
     """Holder for elements of Modron's configuration that can change during runtime
     or need to be persistent across restarts"""
 
-    library_version: str = Field(None, description='Version of Modron last booted')
+    library_version: Optional[str] = Field(None, description='Version of Modron last booted')
     reminder_time: Dict[int, datetime] = Field(None, description='Next time to check if a reminder is needed')
     last_message: Dict[int, LastMessage] = Field(default_factory=dict, description='Information about the last message')
     characters: Dict[int, Dict[int, str]] = Field(default_factory=dict,
                                                   description='Character being played by each player')
 
-    @validator('reminder_time')
+    @field_validator('reminder_time', mode='after')
+    @classmethod
     def convert_str_to_int(cls, value: Optional[Dict]):
         if value is not None:
             return dict((int(k), v) for k, v in value.items())
@@ -74,7 +75,7 @@ class ModronState(BaseModel):
         """
         with open(path, 'r') as fp:
             data = yaml.load(fp, yaml.SafeLoader)
-            return ModronState.parse_obj(data)
+            return ModronState.model_validate(data)
 
     def get_active_character(self, guild_id: int, player_id: int) -> tuple[str, Character, Path]:
         """Get the active character for a player
@@ -113,5 +114,5 @@ class ModronState(BaseModel):
         """
         with open(path, 'w') as fp:
             # Convert to JSON so that it uses Pydantic's conversations of special types
-            ready = json.loads(self.json())
+            ready = json.loads(self.model_dump_json())
             yaml.dump(ready, fp, indent=2)
