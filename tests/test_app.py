f89fc70d6b2c05afa1332f2c25bd843263c83c31
Logan Ward <ward.logan.t@gmail.com>
2026-06-30 21:04:36 -0400

    Change how functions are defined
    Signature inspect in discordpy hits different
    with newer versions of Python

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,13 @@
 from multiprocessing import Process
 from time import sleep
 
+from pytest import mark
+
 from modron.app import main
+from modron.bot import ModronClient
+from modron.interact.dice_roll import DiceRollInteraction
+from modron.interact import attach_commands
+from modron.interact.npc import NPCGenerator
 
 
 def test_launch():
@@ -17,3 +23,18 @@ def test_launch():
     proc.join(timeout=30)
     assert proc.exitcode is not None, 'Still has not terminated'
     assert proc.exitcode in [0, -15], f'Something went awry. Exitcode: {proc.exitcode}'
+
+
+@mark.asyncio
+def test_register_functions(bot: ModronClient):
+    """Ensure that function registration works as advertised"""
+    modules = [DiceRollInteraction(), NPCGenerator()]
+    attach_commands(bot, modules)
+
+    assert len(bot.commands) == 4  # ['roll', 'modron', 'help', 'npcgen']
+    assert bot.all_commands['modron'].signature == '[args...]'
+
+    for module in modules:
+        command = bot.all_commands[module.name]
+        assert command.signature == '[args...]'
+        assert isinstance(command.callback.module, module.__class__)
